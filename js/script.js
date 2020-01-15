8b06c040d1171fe4af924daf4c02b022e026a0a7
Brian Buitrago <b.buitrago@hotmail.com>
2020-01-15 03:24:03 -0500

    In Progress

--- js/script.js
+++ js/script.js
@@ -4,39 +4,64 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-//I add global variables that store the DOM elements
+"use strict";
+
+//Here I declare global variables to store the DOM elements
 const studentList = document.getElementsByClassName("student-item cf");
+
 const pageItems = 10;
+const page = Math.ceil(studentList.length / pageItems);
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-const showPage = (list, page) => {
+//This function is simply to hide or display a set of 10 students that are suppose to show for each page link. The functions loops through the Student List.
+const showPage = (studentList, page) => {
   let startIndex = page * pageItems - pageItems;
   let endIndex = page * pageItems;
 
-  for (let i = 0; i < list.length; i += 1) {}
+  for (let i = 0; i < studentList.length; i += 1) {
+    if (i >= startIndex && i <= endIndex) {
+      studentList[i].style.display = "block";
+    } else {
+      studentList[i].style.display = "none";
+    }
+  }
 };
+
+showPage(studentList, 1);
 
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
-const appendPageLinks = () => {};
+const appendPageLinks = studentList => {
+  const pageNumber = Math.ceil(studentList.length / pageItems);
+  const div = document.createElement("div");
+  div.setAttribute("class", "pagination");
+  document.body.appendChild(div);
+  const ul = document.createElement("ul");
+  div.appendChild(ul);
+  for (let i = 0; i < pageNumber; i += 1) {
+    if (i != pageNumber) {
+      let li = document.createElement("li");
+      ul.appendChild(li);
+      const a = document.createElement("a");
+      a.href = "#";
+      a.textContent = i + 1;
+      if (i === 0) {
+        a.setAttribute("class", "active");
+      }
+      li.appendChild(a);
+    }
+  }
+};
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(studentList);
+
+//Dynamic search form, to look for a specific student
+const searchBar = document.createElement("input");
+searchBar.type = "text";
+searchBar.setAttribute("class", "searchInput");
+searchBar.innerText = "Search for a Student";
+document.body.appendChild(searchBar);
 
 //Thank you for taking a look at my code. I am going for the "Exceeds Expectations" grade. If its not on par with that grade, then please reject this project for resubmission.
