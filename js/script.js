383d98b8219cac507578affead729d5b49619440
Brian Buitrago <b.buitrago@hotmail.com>
2020-01-18 04:52:51 -0500

    refactoring, still in progress...

--- js/script.js
+++ js/script.js
@@ -11,6 +11,24 @@ const studentList = document.getElementsByClassName("student-item");
 //This is variable stores how many students I want to show on each page
 const pageItems = 10;
 
+//I create a search form and button dynamically
+
+const searchBar = document.createElement("input");
+searchBar.type = "text";
+searchBar.setAttribute("class", "searchInput");
+searchBar.placeholder = "Search Students...";
+
+const searchButton = document.createElement("button");
+searchButton.type = "button";
+searchButton.setAttribute("class", "searchBtn");
+searchButton.innerHTML = "Search";
+
+//I append the search form and search button to the HTML document
+
+const divHeader = document.querySelector(".page-header");
+divHeader.appendChild(searchButton);
+divHeader.appendChild(searchBar);
+
 //This function is simply to hide or display a set of 10 students that are suppose to show for each page link. The functions loops through the Student List.
 const showPage = (studentList, page) => {
   let startIndex = page * pageItems - pageItems;
@@ -50,56 +68,62 @@ const appendPageLinks = studentList => {
       li.appendChild(a);
     }
   }
+  //Click event listener to loop through all the page links and bring up the associated number of students for that page number
+
+  const clicked = document.querySelectorAll("a");
+
+  for (let i = 0; i < clicked.length; i++) {
+    clicked[i].addEventListener("click", evt => {
+      if (event.target.className === "A") {
+        if (event.target.className === "active") {
+          event.target.classList.remove("active");
+        }
+      } else {
+        let pageNumber = clicked[i].innerHTML;
+        event.target.classList.add("active");
+        showPage(studentList, pageNumber);
+      }
+    });
+  }
 };
 
 //Call on the appendPageLinks function with the studentList variable as the parameter
 appendPageLinks(studentList);
 
-//Click event listener to loop through all the page links and bring up the associated number of students for that page number
+//I build a function for the Search Form, in order to filter the students by letters or keywords
+const studentArray = [];
 
-const linkClick = studentList => {
-  const clicker = document.getElementsByTagName("a");
-  for (let i = 0; i < clicker.length; i++) {
-    clicker[i].addEventListener("click", event => {
-      if (event.target.tagName === "a") {
-        if (event.target.className === "active") {
-          clicker[i].classList.remove("active");
-        }
+const search = () => {
+  const term = event.target.value.toLowerCase();
+  Array.from(studentList).forEach(function(studentList) {
+    const name = studentList.firstElementChild.textContent;
+    for (let i = 0; i <= name.length; i++) {
+      if (name.toLowerCase().indexOf(term) != -1) {
+        studentList.style.display = "block";
+      } else {
+        studentList.style.display = "none";
       }
-      event.target.classList.add("active");
-      let pageNumber = event.target.innerText;
-      showPage(studentList, pageNumber);
-    });
-  }
-};
-
-linkClick(studentList);
-
-//I create the search form dynamically
-
-const searchBar = document.createElement("input");
-searchBar.type = "text";
-searchBar.setAttribute("class", "searchInput");
-searchBar.placeholder = "Search Students...";
-
-//I attach the search form to the HTML document
-
-const divHeader = document.querySelector(".page-header");
-divHeader.appendChild(searchBar);
-
-//I add functionality to the Search Form in order to filter the students by letters and keywords
-
-searchBar.addEventListener("keyup", function(e) {
-  const term = e.target.value.toLowerCase();
-  const searchStudents = studentList;
-  Array.from(searchStudents).forEach(function(studentList) {
-    const title = studentList.firstElementChild.textContent;
-    if (title.toLowerCase().indexOf(term) != -1) {
-      studentList.style.display = "block";
-    } else {
-      studentList.style.display = "none";
+      // if () {
+      //   let error = document.createElement("div");
+      //   error.id = "error";
+      //   error.innerHTML =
+      //     "Sorry, there are no students with the name  " + term + ".";
+      //   divHeader.appendChild(error);
+      // }
     }
   });
+};
+
+//I invoke my search function with the keyup event listner
+searchBar.addEventListener("keyup", () => {
+  search();
 });
+
+//Added an event listener to my search button
+searchButton.addEventListener("click", () => {
+  console.log("This button is functional");
+});
+
+//Alert message if there are no search results in the search form
 
 //Thank you for taking a look at my code. I am going for the "Exceeds Expectations" grade. If its not on par with that grade, then please reject this project for resubmission.
